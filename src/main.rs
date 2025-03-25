67f9abbda63ad8eb4fc626dbb43a4be1077277bc
Liquid369 <45834289+Liquid369@users.noreply.github.com>
2025-03-25 10:47:57 -0500

    Check for forks in peers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod message;
 mod node;
 mod utils;
+mod webhook;
 
-use node::{Ip, Node};
+use node::{Ip, Node, check_and_alert_forks};
 use std::error::Error;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
+use webhook::Webhook;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -13,6 +15,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
         ip: Ip::Ip4("194.195.87.248".to_string()),
     };
     let peers = node.get_peers().await?;
-    println!("found {:?}", peers);
+
+    let webhook = Webhook::new("https://discord.com/api/webhooks/1352221072208166952/W_TaVpQ2jxYD9s63YBHo0S-vkCnf7SVvzvYWU8OcEZb5tANHm7IecFTNRlu9TnAtG0-u");
+    // Check peers found for hashes and compare for potential forks
+    check_and_alert_forks(&peers, &webhook).await?;
     Ok(())
 }
